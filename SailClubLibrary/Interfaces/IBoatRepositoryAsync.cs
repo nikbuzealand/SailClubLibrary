3575dc15644fc614cf879aeae4835db2eb473707
william19431 <whjortmeincke@gmail.com>
2026-04-07 11:12:15 +0200

    interface

---
+++ SailClubLibrary/Interfaces/IBoatRepositoryAsync.cs
@@ -0,0 +1,21 @@
+﻿using SailClubLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailClubLibrary.Interfaces
+{
+    public interface IBoatRepositoryAsync
+    {
+        Task<int> Count { get; }
+   
+       Task <List<Boat>> GetAllBoats();
+        Task AddBoat(Boat boat);
+        Task RemoveBoat(string sailNumber);
+        Task UpdateBoat(Boat boat);
+       Task< Boat?> SearchBoat(string sailNumber);
+       Task <List<Boat>> FilterBoats(string filterCriteria);
+    }
+}
---
+++ SailClubLibrary/Interfaces/IBookingRepositoryAsync.cs
@@ -0,0 +1,20 @@
+﻿using SailClubLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailClubLibrary.Interfaces
+{
+    public interface IBookingRepositoryAsync
+    {
+        Task AddBooking(Booking booking);
+        Task RemoveBooking(Booking b);
+      Task  <List<Booking>> GetAllBookings();
+        Task UpdateBooking(int id, Booking newBooking);
+        Task PrintAll();
+       Task <int> GetBookingCountForMember(Member member);
+        Task <Dictionary<string, int>> GetAllBookingsForMembers();
+    }
+}
