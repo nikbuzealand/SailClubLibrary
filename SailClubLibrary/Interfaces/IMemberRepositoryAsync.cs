8e7046442d6d6322e192513525173898c8082fc2
william19431 <whjortmeincke@gmail.com>
2026-04-07 11:04:46 +0200

    asynci nterface

---
+++ SailClubLibrary/Interfaces/IMemberRepositoryAsync.cs
@@ -0,0 +1,21 @@
+﻿using SailClubLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailClubLibrary.Interfaces
+{
+    public interface IMemberRepositoryAsync
+    {
+        Task<int> count { get; }
+        Task Addmemberasync(Member amember);
+        Task Removemember(Member amember);
+        Task Updatemember(Member amember);
+        Task<List<Member>> GetAllMembers();
+        Task printall();
+        Task<Member?> Searchmember(string number);
+        Task<List<Member>> Filtermembers(string filtercriteria, string filtertype);
+    }
+}
---
+++ SailClubLibrary/Services/MemberRepositoryAsync.cs
@@ -0,0 +1,50 @@
+﻿using SailClubLibrary.Interfaces;
+using SailClubLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailClubLibrary.Services
+{
+    internal class MemberRepositoryAsync : IMemberRepository
+    {
+        public int Count => throw new NotImplementedException();
+
+        public void AddMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> FilterMembers(string filterCriteria, string filterType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> GetAllMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveMember(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member? SearchMember(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
